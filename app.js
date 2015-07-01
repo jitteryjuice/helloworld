f8727a1ba999034b35b9a253f77614702a55c416
Jeremy Shade <jeremy.shade@outlook.com>
2015-07-01 19:39:27 +0800

    added bootstrap, changed headings. Different error..

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 if(typeof require !== 'undefined') XLSX = require('xlsx');
-var workbook = XLSX.readFile('test.xlsx');
+//var workbook = XLSX.readFile('test.xlsx');
 /* DO SOMETHING WITH workbook HERE */
 
 /* set up drag-and-drop event */
 function handleDrop(e) {
+  console.log("Hello :)");
   e.stopPropagation();
   e.preventDefault();
-  var files = e.dataTransfer.files;
-  var i,f;
-  for (i = 0, f = files[i]; i != files.length; ++i) {
-    var reader = new FileReader();
-    var name = f.name;
-    reader.onload = function(e) {
-      var data = e.target.result;
-
-      /* if binary string, read with type 'binary' */
-      var workbook = XLSX.read(data, {type: 'binary'});
-
-      /* DO SOMETHING WITH workbook HERE */
-    };
-    reader.readAsBinaryString(f);
-  }
+  console.log(e.dataTranswer.files);
+  // var files = e.dataTransfer.files;
+  // var i,f;
+  // for (i = 0, f = files[i]; i != files.length; ++i) {
+  //   var reader = new FileReader();
+  //   var name = f.name;
+  //   reader.onload = function(e) {
+  //     var data = e.target.result;
+  //
+  //     /* if binary string, read with type 'binary' */
+  //     var workbook = XLSX.read(data, {type: 'binary'});
+  //     /* DO SOMETHING WITH workbook HERE */
+  //   };
+  //   reader.readAsBinaryString(f);
+  // }
 }
-drop.addEventListener('drop', handleDrop, false);
+var drop1 = document.getElementById("test");
+drop1.addEventListener('drop', handleDrop, false);
